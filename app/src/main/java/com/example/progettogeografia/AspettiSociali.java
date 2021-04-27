ec850df122c8e11709bf672fb07b1b3b472102dd
davide.guarinoni <davide.guarinoni@my.issm.it>
2021-04-27 10:59:22 +0200

    AspettiSociali

---
+++ app/src/main/java/com/example/progettogeografia/AspettiSociali.java
@@ -0,0 +1,14 @@
+package com.example.progettogeografia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AspettiSociali extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aspetti_sociali);
+    }
+}
